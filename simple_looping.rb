620a2245bc2519c44a4baf18075225780ea798de
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-23 18:16:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -59,7 +59,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  for current_iteration in number_of_times
+  for current_iteration in (1..number_of_times)
     puts phrase
   end
   
